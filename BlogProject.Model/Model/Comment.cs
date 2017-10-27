619c801a5cdcb1526d8057ca2a04e1e58f160e58
Luiz Assis <luizaaca@gmail.com>
2017-10-27 17:05:13 -0200

    Database and models

---
+++ BlogProject.Model/Model/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.ActiveRecord;
+
+namespace BlogProject.Model.Model
+{
+    [ActiveRecord("Comments")]
+    public class Comment : ActiveRecordBase<Comment>
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        [BelongsTo("PostId")]
+        public Post Post { get; set; }
+        [Property]
+        public string Text { get; set; }
+        [Property]
+        public string Author { get; set; }
+        [Property]
+        public DateTime DateAdded { get; set; }
+    }
+}
---
+++ BlogProject.Model/Model/Post.cs
@@ -0,0 +1,41 @@
+﻿using Castle.ActiveRecord;
+using Castle.ActiveRecord.Queries;
+using System;
+using System.Collections.Generic;
+
+namespace BlogProject.Model.Model
+{
+    [ActiveRecord("Posts")]
+    public class Post : ActiveRecordBase<Post>
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        [Property]
+        public string Subject { get; set; }
+        [Property]
+        public string Text { get; set; }
+
+        public string ShortText
+        {
+            get
+            {
+                if (Text.Length > 20)
+                    return Text.Substring(0, 20) + "...";
+                else
+                    return Text;
+            }
+        }
+
+        [HasMany]
+        public IList<Comment> Comments { get; set; }
+        [Property]
+        public DateTime DateAdded { get; set; }
+
+        public static Post FindLatestPost()
+        {
+            SimpleQuery<Post> q = new SimpleQuery<Post>(@"from Post p order by p.DateAdded desc");
+
+            return q.Execute()[0];
+        }
+    }
+}
